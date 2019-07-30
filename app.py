10efa6cd4ed90052f22d8b0ca55040ddb71d175b
myname21-meet <myname21@meet.mit.edu>
2019-07-30 05:14:59 -0400

    day3l1

--- app.py
+++ app.py
@@ -4,7 +4,11 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "home page" 
+    return render_template("home.html")
+@app.route('/hello')
+def hello_route():
+	return render_template('hello.html' , n='Ido')
 
+       
 if __name__ == '__main__':
     app.run(debug=True)
